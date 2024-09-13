2a2bc9c0346add6a9d522c639badff01e5479db9
Mariana Amina <mariana.dantasweb7@gmail.com>
2024-09-13 12:27:51 -0300

    Lesson 32 (and the end of project)

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,21 +1,30 @@
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import useFetch from './useFetch';
+
 
 const BlogDetails = () => {
     const { id } = useParams()
-
     const {data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(() => history.push('/'))
+    }
 
     return ( 
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
             {error && <div>{error}</div>}
             { blog && 
-                <div>
+                <article>
                     <h2>{blog.title} </h2>
                     <p>Writen by <span>{blog.author}</span></p>
                     <p>{blog.body}</p>
-                </div>}
+                    <button onClick={handleClick}>Delete</button>
+                </article>
+            }
         </div>
      );
 }
