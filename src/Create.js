27a37428bfc70320443c88b7d6ce944d115735b9
Mariana Amina <mariana.dantasweb7@gmail.com>
2024-09-13 12:17:50 -0300

    lesson 30

--- src/Create.js
+++ src/Create.js
@@ -1,26 +1,65 @@
+import { useState } from "react";
+import {useHistory} from 'react-router-dom'
+
+
 const Create = () => {
-    return ( 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
+
+        //history.go(-1); //going back one page in history
+        history.push('/');
+    }
+
+    return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
-                <input 
+                <input
                     type="text"
                     required
-                 />
-                 <label>Blog content:</label>
-                <textarea 
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog content:</label>
+                <textarea
                     required
-                 ></textarea>
-                 <label>Blog author:</label>
-                <select>
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
+                {/* When a button is pressed inside a form, it trigger a submit event (even if it is not a submit button as in HTML) */}
             </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
